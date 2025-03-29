375854c6d42fdb4e1fe15e5e37e48557eee5d9ae
fakhrirrahman <fakhrirrahman7@gmail.com>
2025-03-30 03:24:27 +0700

    fix(auth): allow filament access for all users

--- app/Models/User.php
+++ app/Models/User.php
@@ -50,6 +50,6 @@ class User extends Authenticatable implements FilamentUser
 
     public function canAccessPanel(Panel $panel): bool
     {
-        return str_ends_with($this->email, 'fakhrirrahman7@gmail.com') && $this->hasVerifiedEmail();
+        return true;
     }
 }
