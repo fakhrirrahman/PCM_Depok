4470a6c701dd798531f27af23522b62aa7cfa7b8
fakhrirrahman <fakhrirrahman7@gmail.com>
2025-04-16 17:41:20 +0700

    update feat

--- database/seeders/StrukturOrganisasiSeeder.php
+++ database/seeders/StrukturOrganisasiSeeder.php
@@ -9,9 +9,6 @@ use Illuminate\Support\Str;
 
 class StrukturOrganisasiSeeder extends Seeder
 {
-    /**
-     * Run the database seeds.
-     */
     public function run(): void
     {
         StrukturOrganisasi::insert([
