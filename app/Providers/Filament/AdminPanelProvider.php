c0813bf133834c302cac38aae88a53ef56cbf0d2
fakhrirrahman <fakhrirrahman7@gmail.com>
2025-04-26 15:52:21 +0700

    refactor(filament): remove anggota count widget

--- app/Providers/Filament/AdminPanelProvider.php
+++ app/Providers/Filament/AdminPanelProvider.php
@@ -18,7 +18,6 @@ use Illuminate\Foundation\Http\Middleware\VerifyCsrfToken;
 use Illuminate\Routing\Middleware\SubstituteBindings;
 use Illuminate\Session\Middleware\StartSession;
 use Illuminate\View\Middleware\ShareErrorsFromSession;
-use App\Filament\Resources\AnggotaResource\Widgets\AnggotaCountWidget;
 use App\Filament\Pages\CustomDashboard;
 
 class AdminPanelProvider extends PanelProvider
