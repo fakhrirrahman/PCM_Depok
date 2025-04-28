26244bc83114b6b2f9e8b067c1a0f1165973ed2c
fakhrirrahman <fakhrirrahman7@gmail.com>
2025-04-29 03:34:16 +0700

    style(keuangan): remove unnecessary comments

--- app/Filament/Resources/KeuanganResource/Widgets/KeuanganChart.php
+++ app/Filament/Resources/KeuanganResource/Widgets/KeuanganChart.php
@@ -13,19 +13,18 @@ class KeuanganChart extends ChartWidget
     {
         // Ambil data pengeluaran
         $pengeluaranData = Keuangan::selectRaw('kategori as nama_kategori, COUNT(*) as total')
-            ->where('tipe', 'pengeluaran')  // Filter berdasarkan tipe pengeluaran
+            ->where('tipe', 'pengeluaran')  
             ->groupBy('kategori')
             ->pluck('total', 'nama_kategori')
             ->toArray();
         
         // Ambil data pemasukan
         $pemasukanData = Keuangan::selectRaw('kategori as nama_kategori, COUNT(*) as total')
-            ->where('tipe', 'pemasukan')  // Filter berdasarkan tipe pemasukan
+            ->where('tipe', 'pemasukan') 
             ->groupBy('kategori')
             ->pluck('total', 'nama_kategori')
             ->toArray();
 
-        // Gabungkan data pemasukan dan pengeluaran
         $mergedData = array_merge($pengeluaranData, $pemasukanData);
 
         return [
@@ -33,24 +32,24 @@ class KeuanganChart extends ChartWidget
                 [
                     'label' => 'Pemasukan',
                     'data' => array_values($pemasukanData),
-                    'backgroundColor' => 'rgba(75, 192, 192, 0.2)', // Warna untuk pemasukan
+                    'backgroundColor' => 'rgba(75, 192, 192, 0.2)', 
                     'borderColor' => 'rgba(75, 192, 192, 1)',
                     'borderWidth' => 1,
                 ],
                 [
                     'label' => 'Pengeluaran',
                     'data' => array_values($pengeluaranData),
-                    'backgroundColor' => 'rgba(255, 99, 132, 0.2)', // Warna untuk pengeluaran
+                    'backgroundColor' => 'rgba(255, 99, 132, 0.2)', 
                     'borderColor' => 'rgba(255, 99, 132, 1)',
                     'borderWidth' => 1,
                 ]
             ],
-            'labels' => array_keys($mergedData), // Gabungkan kategori
+            'labels' => array_keys($mergedData), 
         ];
     }
 
     protected function getType(): string
     {
-        return 'bar'; // Gunakan chart tipe bar
+        return 'bar'; 
     }
 }
