ebd7b78f8d7d810b23f96e936f352ce3f6d93902
fakhrirrahman <fakhrirrahman7@gmail.com>
2025-05-11 00:26:40 +0700

    update delete coment tentang-kami

--- resources/views/pages/tentang-kami.blade.php
+++ resources/views/pages/tentang-kami.blade.php
@@ -4,8 +4,6 @@
 
 @section('content')
 @include('components.page-title', ['title' => 'Tentang Kami', 'current' => 'Tentang Kami'])
-{{-- @include('components.about-section') --}}
 @include('components.visi-misi-section')
-{{-- @include('components.member-table-about') --}}
 
 @endsection
