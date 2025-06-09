1a6a9a79382869510bf6896ddbe9bfca599ab6df
fahrir <fakhrirrahman7@gmail.com>
2025-06-09 17:48:11 +0700

    Fix KegiatanController index method to retrieve the latest 3 Kegiatan entries up to today

--- app/Http/Controllers/KegiatanController.php
+++ app/Http/Controllers/KegiatanController.php
@@ -10,9 +10,10 @@ class KegiatanController extends Controller
 {
     public function index()
     {
-        $kegiatans = Kegiatan::whereDate('tanggal', '==', '2025' . Carbon::now()->year)
-            ->latest()
-            ->get();
+        $kegiatans = Kegiatan::whereDate('tanggal', '<=', Carbon::today())
+        ->orderBy('tanggal', 'desc') 
+        ->take(3)
+        ->get();
         return view('pages.index', compact('kegiatans'));
     }
     public function kegiatan(Request $request)
